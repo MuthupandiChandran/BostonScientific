c4c6bb73bd697ee39f10bdd1db5d085fe2b31a1f
Muthupandi Chandran <muthupandi@geogo.in>
2023-07-20 14:54:47 +0530

    UI portion for the Admin user-Changeset 1

--- BostonScientificAVS/BostonScientificAVS/Controllers/HomeController.cs
+++ BostonScientificAVS/BostonScientificAVS/Controllers/HomeController.cs
@@ -1,17 +1,24 @@
 ﻿using BostonScientificAVS.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
 using System.Diagnostics;
+
 
 namespace BostonScientificAVS.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private  ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
+
+    
 
         public IActionResult Index()
         {
@@ -47,6 +54,72 @@ namespace BostonScientificAVS.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult Users()
+        {
+           /* ApplicationUser user = new ApplicationUser()*/;
+            var employees = _context.Users.ToList();
+            List<ApplicationUser> users = new List<ApplicationUser>();
+            if (employees != null)
+            {
+                foreach (var user in employees)
+                {
+                    var ApplicationUser = new ApplicationUser()
+                    {
+                        EmpID = user.EmpID,
+                        UserFullName = user.UserFullName,
+                        UserRole = user.UserRole
+
+                    };
+                    users.Add(ApplicationUser);
+
+                }
+                return View(users);
+            }
+            return View(users);
+        }
+
+
+
+
+
+
+
+
+        [HttpPost]
+        public JsonResult SaveUser(ApplicationUser user)
+        {
+            
+                var existingRecord = _context.Users.FirstOrDefault(u => u.EmpID == user.EmpID);
+                if (existingRecord != null)
+                {
+                    existingRecord.UserFullName = user.UserFullName;
+                    existingRecord.UserRole = user.UserRole;
+                    _context.SaveChanges();
+                return Json(new { success = true, message = "Updated successfully!" });
+            
+                }
+
+            else
+            {
+                
+                var newRecord = new ApplicationUser()
+                {
+                    EmpID = user.EmpID,
+                    UserFullName = user.UserFullName,
+                    UserRole = user.UserRole
+                };
+                _context.Users.Add(newRecord);
+                _context.SaveChanges();
+                return Json(new { success = true });
+            }
+        }
+
+
+
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ BostonScientificAVS/BostonScientificAVS/Models/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System.Collections.Generic;
+
+namespace BostonScientificAVS.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+        { }
+        public DbSet<ApplicationUser> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>()
+                .HasIndex(u => u.EmpID)
+                .IsUnique();
+        }
+
+
+    }
+
+}
