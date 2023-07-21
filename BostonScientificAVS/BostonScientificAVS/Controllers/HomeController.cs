6b87e2e5af4507100f03d7c1b1874a09abf061c8
@vigneshshankarvs456 <vigneshshankarvs456@gmail.com>
2023-07-21 18:47:25 +0530

    made changes for csv download, import

--- BostonScientificAVS/BostonScientificAVS/Controllers/HomeController.cs
+++ BostonScientificAVS/BostonScientificAVS/Controllers/HomeController.cs
@@ -4,6 +4,9 @@ using System.Diagnostics;
 using BostonScientificAVS.Services;
 using BostonScientificAVS.DTO;
 using Entity;
+using System.Text;
+using CsvHelper;
+using System.Globalization;
 
 namespace BostonScientificAVS.Controllers
 {
@@ -119,6 +122,35 @@ namespace BostonScientificAVS.Controllers
             
         }
 
+        [HttpGet]
+        public IActionResult DownloadCSVItems()
+        {
+            // Create an empty record with only header column values
+            var headerRecord = new CsvHeader
+            {
+                GTIN = "GTIN",
+                Catalog_Num = "Catalog_Num",
+                Shelf_Life = "Shelf_Life",
+                Label_Spec = "Label_Spec",
+                IFU = "IFU",
+                Edit_Date_Time = "Edit_Date_Time",
+                Edit_By = "Edit_By",
+                Created = "Created",
+                Created_by = "Created_By"
+                // Add more header column values as needed
+            };
+
+            var memoryStream = new MemoryStream();
+            using (var writer = new StreamWriter(memoryStream, Encoding.UTF8, 1024, true))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecord(headerRecord);
+                csv.NextRecord(); // Write a newline to complete the header row
+            }
+
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            return File(memoryStream, "text/csv", "users.csv");
+        }
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ BostonScientificAVS/BostonScientificAVS/Models/CsvHeader.cs
@@ -0,0 +1,16 @@
+﻿
+namespace BostonScientificAVS.Models
+{
+    public class CsvHeader
+    {
+        public string GTIN { get; set; }
+        public string? Catalog_Num { get; set; }
+        public string? Shelf_Life { get; set; }
+        public string? Label_Spec { get; set; }
+        public string? IFU { get; set; }
+        public string? Edit_Date_Time { get; set; }
+        public string? Edit_By { get; set; }
+        public string? Created { get; set; }
+        public string? Created_by { get; set; }
+    }
+}
